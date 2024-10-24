68c733f6f2f65faf4c18960b2904dc6390801892
mbmaciel <mbmaciel@outlook.com>
2024-10-24 10:38:11 -0300

    ajustes

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require('express');
 const puppeteer = require('puppeteer');
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
 // Define a rota principal
 app.get('/scrape', async (req, res) => {
